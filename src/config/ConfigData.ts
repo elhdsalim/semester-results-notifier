bc198248b7743bb8935fae99d0c112677c428e53
slmhd <salim.elhadad.etu@univ-lille.fr>
2026-01-02 20:03:43 +0100

    Add configuration class

---
+++ src/config/ConfigData.ts
@@ -0,0 +1,25 @@
+export class Config {
+    public readonly checkIntervalMs: number;
+
+    public readonly viewportWidth: number;
+
+    public readonly viewportHeight: number;
+
+    public readonly waitDelayMs: number;
+
+    public readonly headless: boolean;
+
+    public readonly referenceFileName: string;
+
+    constructor() {
+        this.checkIntervalMs = 1 * 60 * 1000;
+        this.viewportWidth = 1080;
+        this.viewportHeight = 1024;
+        this.waitDelayMs = 3000;
+        this.headless = false;
+        this.referenceFileName = 'reference.json';
+    }
+    public getCheckIntervalInMinutes(): number {
+        return this.checkIntervalMs / 1000 / 60;
+    }
+}
