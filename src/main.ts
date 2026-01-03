dd42715a07c06e1248a6243028d1c83ee7a5c952
slmhd <salim.elhadad.etu@univ-lille.fr>
2026-01-03 15:28:33 +0100

    Add pushover env vars

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,19 @@ dotenv.config();
 
 const EMAIL = process.env.EMAIL;
 const PASSWORD = process.env.PASSWORD;
+const PUSHOVER_USER_KEY = process.env.PUSHOVER_USER_KEY;
+const PUSHOVER_APP_TOKEN = process.env.PUSHOVER_APP_TOKEN;
 
 if (!EMAIL || !PASSWORD) {
     console.error('Error: please define EMAIL and PASSWORD in your .env');
     process.exit(1);
 }
 
-const monitor = new Monitor(EMAIL, PASSWORD);
+if (!PUSHOVER_USER_KEY || !PUSHOVER_APP_TOKEN) {
+    console.warn('Error: please define PUSHOVER_USER_KEY and PUSHOVER_APP_TOKEN in your .env');
+}
+
+const monitor = new Monitor(EMAIL, PASSWORD, PUSHOVER_USER_KEY, PUSHOVER_APP_TOKEN);
 
 monitor.start().catch((error) => {
     console.error('Error:', error);
